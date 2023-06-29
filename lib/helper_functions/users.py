1702f02d9de1805b2a96cde505265d31b6911bdf
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-06-29 00:13:35 -0400

    updates

--- lib/helper_functions/users.py
+++ lib/helper_functions/users.py
@@ -15,9 +15,15 @@ def create():
     session.commit()
     print("Successfully created a user!")
 
+def edit():
+    print("lets edit something")
+
 def view():
    all = session.query(User).all()
    print(all)
+
+def delete():
+    print("lets delete something")
 
 
 def module():
@@ -26,15 +32,21 @@ def module():
         print(f'''
             Where would you like to go?
             1 - Create user
-            2 - View users 
-            3 - Exit 
+            2 - Edit user
+            3 - View users 
+            4 - Delete user
+            5 - Exit 
         ''')
         user_choice = int(input("Please enter your choice: "))
         if user_choice == 1:
             create()
         elif user_choice == 2:
-            view()
+            edit()
         elif user_choice == 3:
+            view()
+        elif user_choice == 4:
+            delete()
+        elif user_choice == 5:
             return print('Going back to main menu')
         else: 
             print("Invalid choice. Please try again.")
