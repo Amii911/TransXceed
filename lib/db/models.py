fec114bb13dc32d0d325ce151795edf67dfb3b1c
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-07-01 14:04:01 -0400

    updates to models

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
